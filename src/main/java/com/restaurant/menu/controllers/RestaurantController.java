4ab3c59e0081f0ff30b80476843f5b42a270d903
Prathik Kulkarni <kulkarniprathik@gmail.com>
2018-02-08 00:54:18 -0600

    MenuItems api added

--- src/main/java/com/restaurant/menu/controllers/RestaurantController.java
+++ src/main/java/com/restaurant/menu/controllers/RestaurantController.java
@@ -3,6 +3,7 @@ package com.restaurant.menu.controllers;
 import javax.validation.Valid;
 
 import com.restaurant.menu.models.Menu;
+import com.restaurant.menu.models.MenuItem;
 import com.restaurant.menu.models.Restaurant;
 import com.restaurant.menu.repository.RestaurantRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -69,6 +70,58 @@ public class RestaurantController {
         }
     }
 
+    @PostMapping(value="/restaurants/{rid}/menu/{id}/item")
+    public MenuItem addRestaurantMenuItem(@PathVariable("rid") String rid, @PathVariable("id") String id, @Valid @RequestBody MenuItem item ) {
+        Restaurant restaurant = restaurantRepository.findOne(rid);
+        List<Menu> menus;
+        if(restaurant != null) {
+            menus = restaurant.getMenu();
+            if(menus != null) {
+                Iterator<Menu> itr = menus.iterator();
+                while(itr.hasNext()) {
+                    Menu menu = itr.next();
+                    if (menu.getId().equals(id))
+                    {
+                        List<MenuItem> menuItems = menu.getMenuItems();
+                        if(menuItems == null)
+                            menuItems = new ArrayList<>();
+                        menuItems.add(item);
+                        menu.setMenuItems(menuItems);
+                        menus.add(menu);
+                        restaurant.setMenu(menus);
+                        restaurantRepository.save(restaurant);
+                        return item;
+                    }
+                }
+
+            }
+        }
+        return null;
+    }
+
+    @GetMapping("/restaurants/{rid}/menu/{id}/item")
+    public ResponseEntity<List<MenuItem>> getAllMenuItems(@PathVariable("rid") String rid, @PathVariable("id") String id) {
+        List<MenuItem> menuItems=null;
+        Restaurant restaurant = restaurantRepository.findOne(rid);
+        if(restaurant == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        List<Menu> menus = restaurant.getMenu();
+        if(menus == null){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } else {
+                Iterator<Menu> itr = menus.iterator();
+                while(itr.hasNext()) {
+                    Menu menu = itr.next();
+                    if (menu.getId().equals(id)) {
+                        menuItems = menu.getMenuItems();
+                    }
+                }
+            return new ResponseEntity<>(menuItems, HttpStatus.OK);
+        }
+    }
+
+
     @DeleteMapping(value="/restaurants/{id}")
     public void deleteRestaurant(@PathVariable("id") String id) {
         restaurantRepository.delete(id);
@@ -84,8 +137,10 @@ public class RestaurantController {
                 Iterator<Menu> itr = menus.iterator();
                 while(itr.hasNext()) {
                     Menu menu = itr.next();
-                    if (menu.getId().equals(id))
+                    if (menu.getId().equals(id)) {
                         itr.remove();
+                        break;
+                    }
                 }
                 restaurant.setMenu(menus);
                 restaurantRepository.save(restaurant);
